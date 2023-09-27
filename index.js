618cd91c3bd0988339814188fb798c98236b15a5
SoulLess999 <amoghpnivas@gmail.com>
2023-09-28 00:57:53 +0530

    remove cors

--- index.js
+++ index.js
@@ -7,12 +7,12 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(
-  cors({
-    credentials: true,
-    origin: "https://formaker-6ib3.vercel.app",
-  })
-);
+// app.use(
+//   cors({
+//     credentials: true,
+//     origin: "https://formaker-6ib3.vercel.app",
+//   })
+// );
 const PORT = process.env.PORT || 8080;
 const postgre = require("./database");
 
