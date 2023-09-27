054194b71cbe87d349015ed473d5968b16e477ef
SoulLess999 <amoghpnivas@gmail.com>
2023-09-28 01:14:26 +0530

    adding back db code

--- index.js
+++ index.js
@@ -14,66 +14,66 @@ app.use(express.urlencoded({ extended: false }));
 //   })
 // );
 const PORT = process.env.PORT || 8080;
-// const postgre = require("./database");
+const postgre = require("./database");
 
 app.get("/", (req, res) => {
   return res.status(200).send("Simple API homepage");
 });
 
-// app.get("/", async (req, res) => {
-//   try {
-//     const { rows } = await postgre.query("select * from books");
-//     return res.status(200).json({ msg: "OK", data: rows });
-//   } catch (error) {
-//     return res.status(400).json({ msg: error.msg });
-//   }
-// });
+app.get("/", async (req, res) => {
+  try {
+    const { rows } = await postgre.query("select * from books");
+    return res.status(200).json({ msg: "OK", data: rows });
+  } catch (error) {
+    return res.status(400).json({ msg: error.msg });
+  }
+});
 
-// app.get("/:id", async (req, res) => {
-//   try {
-//     const { id } = req.params;
-//     const { rows } = await postgre.query(
-//       "select * from books where book_id = $1",
-//       [id]
-//     );
+app.get("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { rows } = await postgre.query(
+      "select * from books where book_id = $1",
+      [id]
+    );
 
-//     if (rows[0]) {
-//       return res.json({ msg: "OK", data: rows });
-//     }
+    if (rows[0]) {
+      return res.json({ msg: "OK", data: rows });
+    }
 
-//     return res.status(404).json({ msg: "not found" });
-//   } catch (error) {
-//     return res.json({ msg: error.msg });
-//   }
-// });
+    return res.status(404).json({ msg: "not found" });
+  } catch (error) {
+    return res.json({ msg: error.msg });
+  }
+});
 
-// app.post("/", async (req, res) => {
-//   try {
-//     const { name, price } = req.body;
+app.post("/", async (req, res) => {
+  try {
+    const { name, price } = req.body;
 
-//     const sql = "INSERT INTO books(name, price) VALUES($1, $2) RETURNING *";
+    const sql = "INSERT INTO books(name, price) VALUES($1, $2) RETURNING *";
 
-//     const { rows } = await postgre.query(sql, [name, price]);
+    const { rows } = await postgre.query(sql, [name, price]);
 
-//     return res.json({ msg: "OK", data: rows[0] });
-//   } catch (error) {
-//     return res.json({ msg: error.msg });
-//   }
-// });
+    return res.json({ msg: "OK", data: rows[0] });
+  } catch (error) {
+    return res.json({ msg: error.msg });
+  }
+});
 
-// app.put("/:id", async (req, res) => {
-//   try {
-//     const { name, price } = req.body;
+app.put("/:id", async (req, res) => {
+  try {
+    const { name, price } = req.body;
 
-//     const sql =
-//       "UPDATE books set name = $1, price = $2 where book_id = $3 RETURNING *";
+    const sql =
+      "UPDATE books set name = $1, price = $2 where book_id = $3 RETURNING *";
 
-//     const { rows } = await postgre.query(sql, [name, price, req.params.id]);
+    const { rows } = await postgre.query(sql, [name, price, req.params.id]);
 
-//     return res.json({ msg: "OK", data: rows[0] });
-//   } catch (error) {
-//     return res.json({ msg: error.msg });
-//   }
-// });
+    return res.json({ msg: "OK", data: rows[0] });
+  } catch (error) {
+    return res.json({ msg: error.msg });
+  }
+});
 
 app.listen(PORT, () => console.log(`Server is running on PORT: ${PORT}`));
